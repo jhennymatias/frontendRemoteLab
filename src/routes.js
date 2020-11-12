e9b3afdf2e103dc253ca6caeb8d6872b99ce36a2
jhennymatias <jhennifercristinematias@gmail.com>
2020-11-11 22:09:14 -0300

    correção de bug

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import MainStudent from './pages/mainStudent'
-import WebcamCapture from './pages/teste';
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path = "/mainstudent" component = {MainStudent}/>
-                <Route path = "/web" component = {WebcamCapture}/>
             </Switch>
         </BrowserRouter>
     );
